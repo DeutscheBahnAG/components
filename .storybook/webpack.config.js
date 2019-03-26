f3e4e684bec9155d1111ad9f6b03720a85483312
Janko Hofmann <janko.hofmann@bahn-x.de>
2019-03-26 19:10:09 +0100

    update deprecated webpack config format

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,22 +1,22 @@
 const path = require('path');
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader?sourceMap', 'postcss-loader?sourceMap', 'sass-loader?sourceMap', 'import-glob-loader'],
-        include: path.resolve(__dirname, '../')
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-		    loader: "file-loader"
-      },
-      {
-        test: /\.stories\.jsx?$/,
-        loaders: [require.resolve('@storybook/addon-storysource/loader')],
-        enforce: 'pre',
-      }
-    ]
-  }
+module.exports = async ({ config, mode }) => {
+  config.module.rules.push(...[
+    {
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader?sourceMap', 'postcss-loader?sourceMap', 'sass-loader?sourceMap', 'import-glob-loader'],
+      include: path.resolve(__dirname, '../')
+    },
+    {
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      loader: "file-loader"
+    },
+    {
+      test: /\.stories\.jsx?$/,
+      loaders: [require.resolve('@storybook/addon-storysource/loader')],
+      enforce: 'pre',
+    }
+  ]);
+
+  return config;
 };
