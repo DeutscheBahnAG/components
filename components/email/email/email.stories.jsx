c379d67b9d2d050efef3b65033af9dfbf01113bd
Janko Hofmann <janko.hofmann@bahn-x.de>
2019-08-29 18:56:44 +0200

    feat: Add email body styles

--- components/email/email/email.stories.jsx
+++ components/email/email/email.stories.jsx
@@ -5,16 +5,19 @@ import EmailHeader from '../header/header';
 import EmailFooter from '../footer/footer';
 import EmailPinCode from '../pin-code/pin-code';
 import EmailButton from '../button/button';
+import EmailBody from '../body/body';
 
 const template = (
   <Email preview="Email Template Preview">
     <EmailHeader />
-    <h1>Headline</h1>
-    <p>
-      A paragraph with <a href="https://dbx.bahn-x.de">A link</a>.
-    </p>
-    <EmailPinCode>1234</EmailPinCode>
-    <EmailButton href="https://dbx.bahn-x.de">CTA Button</EmailButton>
+    <EmailBody>
+      <h1>Headline</h1>
+      <p>
+        A paragraph with <a href="https://dbx.bahn-x.de">A link</a>.
+      </p>
+      <EmailPinCode>1234</EmailPinCode>
+      <EmailButton href="https://dbx.bahn-x.de">CTA Button</EmailButton>
+    </EmailBody>
     <EmailFooter />
   </Email>
 );
