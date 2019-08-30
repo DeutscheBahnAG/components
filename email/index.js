d933b88833fa64502243dd2254be8819376d100d
Janko Hofmann <janko.hofmann@bahn-x.de>
2019-08-30 14:36:53 +0200

    chore: Add email kit transpile config

---
+++ email/index.js
@@ -0,0 +1,7 @@
+export { default as render } from './render';
+export { default as Email } from './email';
+export { default as EmailBody } from './body/body';
+export { default as EmailButton } from './button/button';
+export { default as EmailFooter } from './footer/footer';
+export { default as EmailHeader } from './header/header';
+export { default as EmailPincode } from './pincode/pincode';
