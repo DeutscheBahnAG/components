5dc3abf2706064af4b7247d64e5f2551eadc5786
Janko Hofmann <janko.hofmann@bahn-x.de>
2019-08-30 14:28:36 +0200

    fix: Fix style preview for email components

--- email/button/button.stories.jsx
+++ email/button/button.stories.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import EmailButton from './button';
+import withEmail from '../withEmail';
 
 storiesOf('Email / EmailButton', module)
   // .addDecorator(withReadme(buttonReadme))
+  .addDecorator(withEmail)
   .add('default', () => <EmailButton>I am a Button</EmailButton>)
   .add('linked', () => <EmailButton href="https://id.bahn.de/">Go to BahnID</EmailButton>);
