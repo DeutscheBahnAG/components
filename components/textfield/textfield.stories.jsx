f281d2266254764c894836c11f0f3fd122e06b77
Nico Hagenburger <nico@hagenburger.net>
2020-02-20 13:11:17 +0100

    docs: Make Textfield editable in Storybook

--- components/textfield/textfield.stories.jsx
+++ components/textfield/textfield.stories.jsx
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import { storiesOf } from '@storybook/react';
 import withReadme from 'storybook-readme/with-readme';
 import Textfield from './textfield';
 import textfieldReadme from './README.md';
 
+const StatefulTextfield = ({ defaultValue, ...props }) => {
+  const [value, setValue] = useState(defaultValue);
+  return <Textfield {...props} value={value} onChange={event => setValue(event.target.value)} />;
+};
+
+StatefulTextfield.propTypes = {
+  defaultValue: PropTypes.string,
+};
+
+StatefulTextfield.defaultProps = {
+  defaultValue: '',
+};
+
 storiesOf('Components / Textfield', module)
   .addDecorator(withReadme(textfieldReadme))
-  .add('Simple Textfield', () => <Textfield value="Simple Textfield" />)
-  .add('Textfield with placeholder', () => <Textfield placeholder="Placeholder text" />)
+  .add('Simple Textfield', () => <StatefulTextfield defaultValue="Simple Textfield" />)
+  .add('Textfield with placeholder', () => <StatefulTextfield placeholder="Placeholder text" />)
   .add('Disabled Textfield', () => <Textfield value="Disabled" disabled />)
-  .add('Readonly Textfield', () => <Textfield value="Readonly" readonly />)
-  .add('Textarea', () => <Textfield value={'Textarea\nwith\nmany\nlines'} type="textarea" />)
-  .add('Number', () => <Textfield value="123" type="number" />)
-  .add('Number with unit', () => <Textfield value="123" type="number" unit="€" />)
-  .add('Number with unit before', () => <Textfield value="123" type="number" unit="£" />)
-  .add('Text with prefix', () => <Textfield value="bahn_de" type="text" prefix="@" />)
-  .add('Text with suffix', () => <Textfield value="info" type="text" suffix="@bahn.de" />)
-  .add('Email address', () => <Textfield value="info@bahn.de" type="email" />)
-  .add('Phone number', () => <Textfield value="0180 6 996633" type="tel" />)
-  .add('URL', () => <Textfield value="https://www.bahn.de" type="url" />)
-  .add('Password', () => <Textfield value="secret" type="password" />);
+  .add('Readonly Textfield', () => <Textfield value="Readonly" readOnly />)
+  .add('Textarea', () => (
+    <StatefulTextfield defaultValue={'Textarea\nwith\nmany\nlines'} type="textarea" />
+  ))
+  .add('Number', () => <StatefulTextfield defaultValue="123" type="number" />)
+  .add('Number with unit', () => <StatefulTextfield defaultValue="123" type="number" unit="€" />)
+  .add('Number with unit before', () => (
+    <StatefulTextfield defaultValue="123" type="number" unit="£" />
+  ))
+  .add('Text with prefix', () => (
+    <StatefulTextfield defaultValue="bahn_de" type="text" prefix="@" />
+  ))
+  .add('Text with suffix', () => <StatefulTextfield type="text" suffix="@bahn.de" />)
+  .add('Email address', () => <StatefulTextfield defaultValue="info@bahn.de" type="email" />)
+  .add('Phone number', () => <StatefulTextfield defaultValue="0180 6 996633" type="tel" />)
+  .add('URL', () => <StatefulTextfield defaultValue="https://www.bahn.de" type="url" />)
+  .add('Password', () => <StatefulTextfield defaultValue="secret" type="password" />);
