e0b4e0c4ef23e1a4335074daa31353db45eb25be
Nico Hagenburger <nico@hagenburger.net>
2020-03-09 16:46:16 +0100

    docs: Optimize style in Storybook

--- foundation/colors.jsx
+++ foundation/colors.jsx
@@ -9,29 +9,27 @@ const Colors = () => (
     <h2>DBX Colors</h2>
     <p className="sg-code-preview">background-color: dbx-color($name);</p>
     <div className="sg-section">
-      <div className="sg-card-group">
-        {Object.keys(tokens)
-          .filter(key => key.match(/^color\./))
-          .map(color => (
-            <div key={color} className="sg-card-group__item">
-              <StyleguideCard
-                name={color.replace('color.', '')}
-                value={tokens[color]}
-                preview={
-                  <div
-                    style={{
-                      position: 'absolute',
-                      width: '100%',
-                      height: '100%',
-                      top: 0,
-                      left: 0,
-                      backgroundColor: tokens[color],
-                    }}
-                  />
-                }
-              />
-            </div>
-          ))}
+      <div className="sg-card-group sg-card-group--horizontal">
+        {Object.keys(tokens.color).map(color => (
+          <div key={color} className="sg-card-group__item">
+            <StyleguideCard
+              name={color}
+              value={tokens.color[color]}
+              preview={
+                <div
+                  style={{
+                    position: 'absolute',
+                    width: '100%',
+                    height: '100%',
+                    top: 0,
+                    left: 0,
+                    backgroundColor: tokens.color[color]
+                  }}
+                />
+              }
+            />
+          </div>
+        ))}
       </div>
     </div>
   </>
--- foundation/spacings.jsx
+++ foundation/spacings.jsx
@@ -10,31 +10,23 @@ const Spacings = () => (
     <p className="sg-code-preview">margin: dbx-spacing($name);</p>
     <div className="sg-section">
       <div className="sg-card-group">
-        {Object.keys(tokens)
-          .filter(key => key.match(/^spacing\./))
-          .map(name => (
-            <div key={name} className="sg-card-group__item">
-              <StyleguideCard
-                name={name.replace('spacing.', '')}
-                value={tokens[name]}
-                preview={
-                  <div
-                    style={{
-                      position: 'absolute',
-                      width: tokens[name],
-                      height: tokens[name],
-                      top: '50%',
-                      left: '50%',
-                      transform: 'translateX(-50%) translateY(-50%)',
-                      background:
-                        'url(\'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="8px" height="8px"><style>path{stroke-antialiasing:true;fill:red;stroke:%23fcd0d0;stroke-linecap:square;stroke-width:3px}</style><path d="M4 0 0 4"/><path d="M8 4 4 8"/></svg>\')',
-                      border: '1px #E21617 solid',
-                    }}
-                  />
-                }
-              />
-            </div>
-          ))}
+        {Object.keys(tokens.spacing).map(size => (
+          <div key={size} className="sg-card-group__item">
+            <StyleguideCard
+              name={size}
+              value={tokens.spacing[size]}
+              preview={
+                <div
+                  className="sg-spacing"
+                  style={{
+                    width: tokens.spacing[size],
+                    height: tokens.spacing[size]
+                  }}
+                />
+              }
+            />
+          </div>
+        ))}
       </div>
     </div>
   </>
