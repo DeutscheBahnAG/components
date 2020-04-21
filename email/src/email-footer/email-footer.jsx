b27e26453e7fea0c081ae2ed6f86286eadc10500
Janko Hofmann <janko.hofmann@bahn-x.de>
2020-04-21 19:09:19 +0200

    feat: Update default EmailFooter imprint

--- email/src/email-footer/email-footer.jsx
+++ email/src/email-footer/email-footer.jsx
@@ -5,7 +5,7 @@ import EmailSeparator from '../email-separator/email-separator';
 import EmailWrapper from '../email-wrapper/email-wrapper';
 
 const defaultImprint =
-  'Deutsche Bahn AG, Potsdamer Platz 2, 10785 Berlin, AG Berlin-Charlottenburg HRB 50000, USt-IdNr.: DE811569869. Die Deutsche Bahn AG wird vertreten durch den Vorstand: Dr. Richard Lutz, Ronald Pofalla, Berthold Huber, Alexander Doll, Prof. Dr. Sabina Jeschke und Martin Seiler.';
+  'Deutsche Bahn AG, Potsdamer Platz 2, 10785 Berlin, AG Berlin-Charlottenburg HRB 50000, USt-IdNr.: DE811569869. Die Deutsche Bahn AG wird vertreten durch den Vorstand: Dr. Richard Lutz, Ronald Pofalla, Dr. Levin Holle, Berthold Huber, Dr. Sigrid Nikutta, Prof. Dr. Sabina Jeschke und Martin Seiler.';
 
 const EmailFooter = ({ children, imprint }) => {
   return (
