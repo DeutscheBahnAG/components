f092066c35aa370e959034250367499c28339ab4
Nico Hagenburger <nico@hagenburger.net>
2020-05-07 22:39:01 +0200

    docs: Add sizes overview demo page

--- foundation/basics.stories.jsx
+++ foundation/basics.stories.jsx
@@ -2,9 +2,11 @@ import { storiesOf } from '@storybook/react';
 import Colors from './colors';
 import Spacings from './spacings';
 import TextStyles from './text-styles';
+import ComponentSizes from './component-sizes';
 
 storiesOf('Foundation', module)
   .addParameters({ options: { showPanel: false } })
   .add('Colors', Colors)
   .add('Text Styles', TextStyles)
-  .add('Spacings', Spacings);
+  .add('Spacings', Spacings)
+  .add('Component sizes', ComponentSizes);
---
+++ foundation/component-sizes.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import dbxConstants from './dbx-constants';
+import Logo from '../components/logo';
+import Button from '../components/button';
+import Textfield from '../components/textfield';
+
+const { tokens } = dbxConstants;
+
+const ComponentSizes = () => (
+  <>
+    <h2>DBX Component Sizes</h2>
+    <div className="sg-section">
+      <table className="sg-table">
+        <thead>
+          <th>Size</th>
+          <th>Logo</th>
+          <th>Button</th>
+          <th>Secondary</th>
+          <th>Solid</th>
+          <th colSpan="2">Textfield</th>
+        </thead>
+        <tbody>
+          {Object.keys(tokens.size).map(size => (
+            <tr key={size}>
+              <th>
+                <code>
+                  size.{size}
+                  <br />
+                  <small>{tokens.size[size]}</small>
+                </code>
+              </th>
+              <td>
+                <Logo size={size}>Logo</Logo>
+              </td>
+              <td>
+                <Button size={size}>Button</Button>
+              </td>
+              <td>
+                <Button variant="secondary" size={size}>
+                  Button
+                </Button>
+              </td>
+              <td>
+                <Button variant="solid" size={size}>
+                  Button
+                </Button>
+              </td>
+              <td>
+                <Textfield size={size} value="Textfield" />
+              </td>
+              <td>
+                <Textfield size={size} value="123" type="number" prefix="@" unit="€" />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  </>
+);
+
+export default ComponentSizes;
