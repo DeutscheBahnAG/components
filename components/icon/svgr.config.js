f306fda541bd80e125743822c50113cf6e1a80e4
Janko Hofmann <janko.hofmann@bahn-x.de>
2020-05-18 18:04:38 +0200

    feat: Add basic setup WIP

---
+++ components/icon/svgr.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  ext: 'jsx',
+  expandProps: 'end',
+  // prettierConfig: {},
+  // svgoConfig: {},
+  memo: true,
+  ref: true,
+  replaceAttrValues: { '#3f434c': 'currentColor' },
+  svgProps: { className: 'dbx-icon' },
+  titleProp: true,
+  // template:
+  // basicTemplate:
+};
