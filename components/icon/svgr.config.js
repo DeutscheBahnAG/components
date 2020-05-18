75cefebf8365c9926fadf6edb39f8b2a362fd98f
Janko Hofmann <janko.hofmann@bahn-x.de>
2020-05-18 18:52:52 +0200

    feat: Add custom file templates

--- components/icon/svgr.config.js
+++ components/icon/svgr.config.js
@@ -1,13 +1,52 @@
+const path = require('path');
+const camelcase = require('camelcase');
+
+const customTemplate = (
+  { template },
+  opts,
+  { imports, interfaces, componentName: originalComponentName, props, jsx, exports }
+) => {
+  const plugins = ['jsx'];
+  if (opts.typescript) {
+    plugins.push('typescript');
+  }
+
+  // SvgIconName => IconName
+  const componentName = {
+    ...originalComponentName,
+    name: originalComponentName.name.substring(3),
+  };
+
+  const typeScriptTpl = template.smart({ plugins });
+  const ast = typeScriptTpl.ast`${imports}
+${interfaces}
+const ${componentName} = (${props}) => {
+  return ${jsx};
+}
+${exports}
+  `;
+  return ast;
+};
+
+const customIndexTemplate = filePaths => {
+  const exportEntries = filePaths.map(filePath => {
+    const basename = path.basename(filePath, path.extname(filePath));
+    // make export name PascalCase while filename is kebab-case
+    const exportName = camelcase(basename, { pascalCase: true });
+    return `export { default as ${exportName} } from './${basename}'`;
+  });
+  return exportEntries.join('\n');
+};
+
 module.exports = {
   ext: 'jsx',
   expandProps: 'end',
-  // prettierConfig: {},
-  // svgoConfig: {},
   memo: true,
   ref: true,
-  replaceAttrValues: { '#3f434c': 'currentColor' },
-  svgProps: { className: 'dbx-icon' },
+  replaceAttrValues: { '#3f434c': 'currentColor', '#3F434C': 'currentColor' },
+  svgProps: { className: 'dbx-icon', width: '24', height: '24' },
   titleProp: true,
-  // template:
-  // basicTemplate:
+  filenameCase: 'kebab',
+  template: customTemplate,
+  indexTemplate: customIndexTemplate,
 };
