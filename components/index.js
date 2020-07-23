46ddd8f2bb8c34b1892eca74fd3e28e0d1af2dad
Armin Roșu <armin.rosu@bahn-x.de>
2020-07-23 14:17:49 +0200

    feat: Export all Components from src

--- components/index.js
+++ components/index.js
@@ -1,3 +1,16 @@
-/* eslint-disable import/prefer-default-export */
+import Icons from './icon';
+import * as TransportchipIcons from './transportchip/icons';
 
+export { Icons, TransportchipIcons };
 export { default as Button } from './button';
+export { default as Checkbox } from './checkbox';
+export { default as Code } from './code';
+export { default as Loadingindicator } from './loadingindicator';
+export { default as Logo } from './logo';
+export { Pulse } from './logo';
+export { default as Modal } from './modal';
+export { default as Notification } from './notification';
+export { default as Privacyoptions } from './privacyoptions';
+export { default as Select } from './select';
+export { default as Textfield } from './textfield';
+export { default as Transportchip } from './transportchip';
