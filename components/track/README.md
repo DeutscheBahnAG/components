7ce8badce6ec9bc20fed7ab3201bbac6635a5425
Nico Hagenburger <nico@hagenburger.net>
2020-07-28 13:00:01 +0200

    feat: Implement Track component

---
+++ components/track/README.md
@@ -0,0 +1,49 @@
+# Track
+
+Shows the track (platform) number for a trip in the default style of Deutsche Bahn.
+
+This component parses and formats the track information with typographic rules.
+
+## Basic usage
+
+```jsx
+<Track track="1" />
+```
+
+### Fragment
+
+In some train stations two trains can hold the same time on the same track. Usually this track
+is split into two fragments called “a” and “b” (using lowercase characters).
+
+```jsx
+<Track track="2a" />
+```
+
+### Section
+
+This is the parking position of the train in uppercase characters.
+
+```jsx
+<Track track="3 B-E" />
+```
+
+### Prefix
+
+```jsx
+<Track track="Fern 1" />
+```
+
+## Sizing
+
+```jsx
+<Track track="1" size={Track.sizes.M} />
+```
+
+## Translations
+
+```jsx
+<Track
+  track="1"
+  labels={{ platform: 'Platform', platformAbbreviation: 'Pl.' }}
+/>
+```
---
+++ components/track/track.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import parseTrack from './parse-track';
+
+const Track = ({
+  track,
+  size,
+  labels: { platform, platformAbbreviation },
+  className,
+  ...props
+}) => {
+  const { prefix, number, fragment, sectionStart, sectionEnd } = parseTrack(track);
+  return (
+    <span className={clsx('dbx-track', `dbx-track--${size}`, className)} {...props}>
+      {prefix || <abbr title={platform}>{platformAbbreviation}</abbr>} {number}
+      {fragment && <span className="dbx-track__fragment">{fragment}</span>}
+      {sectionStart && (
+        <>
+          {' '}
+          <span className="dbx-track__section">
+            {sectionStart}
+            <span className="dbx-track__separator">–</span>
+            {sectionEnd}
+          </span>
+        </>
+      )}
+    </span>
+  );
+};
+
+Track.sizes = {
+  XS: 'xs',
+  S: 's',
+  M: 'm',
+};
+
+Track.propTypes = {
+  track: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  size: PropTypes.oneOf(Object.keys(Track.sizes).map(k => Track.sizes[k])),
+  labels: PropTypes.shape({
+    platform: PropTypes.string.isRequired,
+    platformAbbreviation: PropTypes.string.isRequired,
+  }),
+};
+
+Track.defaultProps = {
+  className: '',
+  size: Track.sizes.XS,
+  labels: { platform: 'Gleis', platformAbbreviation: 'Gl.' },
+};
+
+export default Track;
