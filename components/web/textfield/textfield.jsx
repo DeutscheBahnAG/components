7382e53ce2cbb55f97fd8ba6e3ef3bbf658580b2
Nico Hagenburger <nico@hagenburger.net>
2020-09-23 16:29:06 +0200

    feat: Forward input ref in Textfield

--- components/web/textfield/textfield.jsx
+++ components/web/textfield/textfield.jsx
@@ -1,96 +1,100 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 
-const noopFn = () => {};
+const noopFn = () => { };
 
 const unitsBeforeField = ['£', 'GBP', '$', 'USD'];
 
-const Textfield = ({
-  onFocus,
-  onBlur,
-  type,
-  size,
-  className,
-  value,
-  unit,
-  prefix,
-  suffix,
-  onChange,
-  inlineLabel,
-  ...otherProps
-}) => {
-  const [isFocused, setFocused] = useState(false);
-  const field = useRef(null);
+const Textfield = React.forwardRef(
+  (
+    {
+      onFocus,
+      onBlur,
+      type,
+      size,
+      className,
+      value,
+      unit,
+      prefix,
+      suffix,
+      onChange,
+      inlineLabel,
+      ...otherProps
+    },
+    fieldRef
+  ) => {
+    const [isFocused, setFocused] = useState(false);
 
-  const handleClick = () => {
-    field.current.focus();
-  };
+    const handleClick = () => {
+      fieldRef.current.focus();
+    };
 
-  const handleFocus = event => {
-    setFocused(true);
-    onFocus(event);
-  };
+    const handleFocus = (event) => {
+      setFocused(true);
+      onFocus(event);
+    };
 
-  const handleBlur = event => {
-    setFocused(false);
-    onBlur(event);
-  };
+    const handleBlur = (event) => {
+      setFocused(false);
+      onBlur(event);
+    };
 
-  const Field = type === 'textarea' ? 'textarea' : 'input';
-  const fieldSize = inlineLabel ? Textfield.sizes.XL : size;
-  let contentBefore = prefix;
-  let contentAfter = suffix;
+    const Field = type === 'textarea' ? 'textarea' : 'input';
+    const fieldSize = inlineLabel ? Textfield.sizes.XL : size;
+    let contentBefore = prefix;
+    let contentAfter = suffix;
 
-  if (unit) {
-    if (unitsBeforeField.includes(unit)) contentBefore = unit;
-    else contentAfter = unit;
+    if (unit) {
+      if (unitsBeforeField.includes(unit)) contentBefore = unit;
+      else contentAfter = unit;
+    }
+
+    const configuredField = (
+      <Field
+        ref={fieldRef}
+        {...otherProps}
+        value={value}
+        type={type === 'textarea' ? null : type}
+        onChange={onChange}
+      />
+    );
+
+    return (
+      <div
+        onClick={handleClick} // Focus on click on the prefix/suffix
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        disabled={otherProps.disabled}
+        readOnly={otherProps.readOnly}
+        className={clsx(
+          'dbx-textfield',
+          `dbx-textfield--${type}`,
+          `dbx-textfield--${fieldSize}`,
+          value && 'dbx-textfield--filled',
+          isFocused && 'dbx-textfield--focus',
+          inlineLabel && 'dbx-textfield--inline-label',
+          className
+        )}
+      >
+        {contentBefore && <span className={clsx('dbx-textfield-prefix')}>{contentBefore}</span>}
+        {(inlineLabel && (
+          // eslint-disable-next-line jsx-a11y/label-has-associated-control
+          <label>
+            <span className={clsx('dbx-textfield__inline-label')}>{inlineLabel}</span>
+            {configuredField}
+          </label>
+        )) ||
+          configuredField}
+
+        {contentAfter && <span className={clsx('dbx-textfield-suffix')}>{contentAfter}</span>}
+      </div>
+    );
   }
-
-  const configuredField = (
-    <Field
-      ref={field}
-      {...otherProps}
-      value={value}
-      type={type === 'textarea' ? null : type}
-      onChange={onChange}
-    />
-  );
-
-  return (
-    <div
-      onClick={handleClick} // Focus on click on the prefix/suffix
-      onFocus={handleFocus}
-      onBlur={handleBlur}
-      disabled={otherProps.disabled}
-      readOnly={otherProps.readOnly}
-      className={clsx(
-        'dbx-textfield',
-        `dbx-textfield--${type}`,
-        `dbx-textfield--${fieldSize}`,
-        value && 'dbx-textfield--filled',
-        isFocused && 'dbx-textfield--focus',
-        inlineLabel && 'dbx-textfield--inline-label',
-        className
-      )}
-    >
-      {contentBefore && <span className={clsx('dbx-textfield-prefix')}>{contentBefore}</span>}
-      {(inlineLabel && (
-        // eslint-disable-next-line jsx-a11y/label-has-associated-control
-        <label>
-          <span className={clsx('dbx-textfield__inline-label')}>{inlineLabel}</span>
-          {configuredField}
-        </label>
-      )) ||
-        configuredField}
-
-      {contentAfter && <span className={clsx('dbx-textfield-suffix')}>{contentAfter}</span>}
-    </div>
-  );
-};
+);
 
 Textfield.sizes = {
   S: 's',
@@ -112,7 +116,7 @@ Textfield.propTypes = {
     'search',
   ]),
   /** The size of the Textfield */
-  size: PropTypes.oneOf(Object.keys(Textfield.sizes).map(k => Textfield.sizes[k])),
+  size: PropTypes.oneOf(Object.keys(Textfield.sizes).map((k) => Textfield.sizes[k])),
   /** Additional class names */
   className: PropTypes.string,
   /** The user input */
