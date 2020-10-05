311ec07d9bebe4557d88c75a5369b70215da5fae
Janko Hofmann <janko.hofmann@bahn-x.de>
2020-10-05 14:27:24 +0200

    feat: Migrate Time to TS

--- components/web/time/helper.ts
+++ components/web/time/helper.ts
@@ -1,2 +1,2 @@
-export default (dateTime) =>
+export default (dateTime: string): number =>
   Date.parse(dateTime.includes('T') ? dateTime : `1970-01-01T${dateTime}`);
--- components/web/time/time.tsx
+++ components/web/time/time.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes, { InferProps } from 'prop-types';
 import clsx from 'clsx';
 import parseTime from './helper';
 
-const formatNumber = (number) => `${number}`.padStart(2, '0');
+const formatNumber = (number: number) => `${number}`.padStart(2, '0');
 
-const Time = ({ dateTime, className, ...props }) => {
+const timePropTypes = {
+  dateTime: PropTypes.string.isRequired,
+  className: PropTypes.string,
+};
+
+type TimeProps = InferProps<typeof timePropTypes>;
+
+const Time: React.FC<TimeProps> = ({ dateTime, className, ...props }) => {
   const date = new Date(parseTime(dateTime));
   return (
     <time className={clsx('dbx-time', className)} dateTime={dateTime} {...props}>
@@ -14,10 +21,7 @@ const Time = ({ dateTime, className, ...props }) => {
   );
 };
 
-Time.propTypes = {
-  dateTime: PropTypes.string.isRequired,
-  className: PropTypes.string,
-};
+Time.propTypes = timePropTypes;
 
 Time.defaultProps = {
   className: '',
