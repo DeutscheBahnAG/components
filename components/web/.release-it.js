0da2d2be5f7d2080a1893a67e9ee62679f8453c7
Armin Roșu <arminrosu@users.noreply.github.com>
2020-10-12 16:27:52 +0200

    fix: Correct commit and tag template (#166)

--- components/web/.release-it.js
+++ components/web/.release-it.js
@@ -1,6 +1,7 @@
 module.exports = {
   git: {
+    commitMessage: 'chore: Release @bahn-x/dbx-web@${version}',
     requireBranch: 'master',
-    tagName: '@bahn-x/dbx-web@{version}',
+    tagName: '@bahn-x/dbx-web@${version}',
   }
 };
