de1bc7d4aaf9829ed21af493769065e0f8bf1e24
Nico Hagenburger <nico@hagenburger.net>
2021-11-11 14:42:13 +0100

    refactor: Order imports alphabetically

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,16 +1,18 @@
 import * as TransportchipIcons from './transportchip/icons';
 
 export { TransportchipIcons };
-export { default as Container } from './container';
+export { default as Anchor } from './anchor';
 export { default as Button } from './button';
 export { default as Checkbox } from './checkbox';
+export { default as Container } from './container';
 export { default as Copy } from './copy';
 export { default as Layout } from './layout';
+export { default as Link } from './link';
 export { default as Loadingindicator } from './loadingindicator';
 export { default as Logo } from './logo';
-export { default as Pulse } from './pulse';
 export { default as Modal } from './modal';
 export { default as Notification } from './notification';
+export { default as Pulse } from './pulse';
 export { default as Select } from './select';
 export { default as Status } from './status';
 export { default as Textfield } from './textfield';
@@ -21,5 +23,3 @@ export { default as Track } from './track';
 export { default as Transportchip } from './transportchip';
 export { default as Triptime } from './triptime';
 export { default as Triptimespan } from './triptimespan';
-export { default as Anchor } from './anchor';
-export { default as Link } from './link';
