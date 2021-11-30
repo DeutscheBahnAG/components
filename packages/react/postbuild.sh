425b4d5f447fc966e1e2ff9ce978056707a255e9
Nico Hagenburger <nico@hagenburger.net>
2021-11-30 15:19:57 +0100

    fix: Include *.d.ts into Node package

--- packages/react/postbuild.sh
+++ packages/react/postbuild.sh
@@ -2,5 +2,6 @@
 
 cp -p package.json dist &&
 for f in `ls *.md`; do cp -p $f dist; done &&
+for f in `ls src/*.d.ts`; do cp -p $f dist; done &&
 for f in `ls src/*/README.md`; do cp -p $f ${f/src/dist}; done &&
 perl -i -pe 's#dist/index#index#' dist/package.json
