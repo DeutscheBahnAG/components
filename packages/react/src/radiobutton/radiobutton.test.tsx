e849001ec2856042f2d0f9d126f93b500b5dee10
Janko Hofmann <janko.j.hofmann@deutschebahn.com>
2022-01-19 15:04:53 +0100

    test: Add tests for Radiobutton

--- packages/react/src/radiobutton/radiobutton.test.tsx
+++ packages/react/src/radiobutton/radiobutton.test.tsx
@@ -1,13 +1,37 @@
 import * as React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import Radiobutton from './radiobutton';
 
 describe('<Radiobutton />', () => {
-  it('should render', () => {
-    render(<Radiobutton />);
-    expect(screen.getByText('Radiobutton', { exact: false })).toBeInTheDocument();
+  it('renders a radio input', () => {
+    const { baseElement } = render(<Radiobutton>Radiobutton Label Text</Radiobutton>);
+    const radioInput = baseElement.querySelector('input[type="radio"]');
+    expect(radioInput).toBeInTheDocument();
   });
 
-  // TODO: add meaningful tests for Radiobutton component
+  it('renders its children inside the input label', () => {
+    const { queryByLabelText } = render(<Radiobutton>Radiobutton Label Text</Radiobutton>);
+    expect(queryByLabelText('Radiobutton Label Text')).toBeInTheDocument();
+  });
+
+  it('accepts an additional className', () => {
+    const { container } = render(
+      <Radiobutton className="test-class">Radiobutton Label Text</Radiobutton>
+    );
+    expect(container.firstChild).toHaveClass('test-class');
+  });
+
+  it('optionally renders footer content', () => {
+    const { queryByText } = render(
+      <Radiobutton footer={<p>Radiobutton Footer Text</p>}>Radiobutton Label Text</Radiobutton>
+    );
+    expect(queryByText('Radiobutton Footer Text')).toBeInTheDocument();
+  });
+
+  it('forwards the input element as ref', () => {
+    const inputRef = React.createRef<HTMLInputElement>();
+    render(<Radiobutton ref={inputRef}>Radiobutton Label Text</Radiobutton>);
+    expect(inputRef.current).toBeInstanceOf(HTMLInputElement);
+  });
 });
