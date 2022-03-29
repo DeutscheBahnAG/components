7f9b8deb434627be4d4e13ca7c8421f383f643bd
Felix Leupold <felix.leupold-extern@deutschebahn.com>
2022-03-29 18:03:28 +0000

    perf: Better Tree Shaking - ES Modules

--- packages/react-icons/postbuild.sh
+++ packages/react-icons/postbuild.sh
@@ -3,3 +3,4 @@
 cp -p package.json ./*.md dist &&
 for f in `ls *.md`; do cp -p $f dist; done &&
 perl -i -pe 's#dist/index#index#' dist/package.json
+perl -i -pe 's#dist/module/index#module/index#' dist/package.json
--- packages/react/postbuild.sh
+++ packages/react/postbuild.sh
@@ -5,3 +5,4 @@ for f in `ls *.md`; do cp -p $f dist; done &&
 for f in `ls src/*.d.ts`; do cp -p $f dist; done &&
 for f in `ls src/*/README.md`; do cp -p $f ${f/src/dist}; done &&
 perl -i -pe 's#dist/index#index#' dist/package.json
+perl -i -pe 's#dist/module/index#module/index#' dist/package.json
